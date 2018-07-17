adf14bdd5c0164597b8741e3f549223624b1bc0c
Hackbright Student <no-reply@hackbrightacademy.com>
2018-07-17 13:03:18 -0700

    Completed exercise

--- server.py
+++ server.py
@@ -18,7 +18,7 @@ AWESOMENESS = [
 def start_here():
     """Home page."""
 
-    return "<!doctype html><html>Hi! This is the home page.</html>"
+    return "<!doctype html><html>Hi! This is the home page.<br><a href='/hello'>Hello page</a></html>"
 
 
 @app.route('/hello')
@@ -34,7 +34,22 @@ def say_hello():
       <body>
         <h1>Hi There!</h1>
         <form action="/greet">
-          What's your name? <input type="text" name="person">
+          What's your name? <input type="text" name="person"><br><br>
+          Choose a compliment! <select name="compliment">
+          	<option value='Awesome'>Awesome</option>
+          	<option value='terrific'>Terrific</option>
+          	<option value='fantastic'>Fantastic</option>
+          	<option value='neato'>Neato</option>
+          	<option value='fantabulous'>Fantabulous</option>
+          	<option value='wowza'>Wowza</option>
+          	<option value='oh-so-not-meh'>Oh-so-not-meh</option>
+          	<option value='brilliant'>Brilliant</option>
+          	<option value='ducky'>Ducky</option>
+          	<option value='coolio'>Coolio</option>
+          	<option value='incredible'>Incredible</option>
+          	<option value='wonderful'>Wonderful</option>
+          	<option value='smashing'>Smashing</option>
+          	<option value='lovely'>Lovely</option>
           <input type="submit" value="Submit">
         </form>
       </body>
@@ -48,7 +63,7 @@ def greet_person():
 
     player = request.args.get("person")
 
-    compliment = choice(AWESOMENESS)
+    compliment = request.args.get("compliment")
 
     return """
     <!doctype html>
